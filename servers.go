01a2b284d5cccce0aa58369d811b8e6bd0524266
Dixon Begay <dixonbegay@gmail.com>
2018-08-22 10:43:55 -0700

    Started working on servers.go WIP

--- servers.go
+++ servers.go
@@ -1,16 +1,44 @@
 package main
 
 import (
+	"strconv"
 	"time"
+
+	"github.com/shadez95/go-arma"
 
 	"github.com/gin-gonic/gin"
 )
 
-type Servers struct {
+type Server struct {
 	ID        int    `gorm:"primary_key"`
 	Name      string `gorm:"unique;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+	arma.Server
+}
+
+func createServer(s *Server) error {
+	db := openDB()
+	defer db.Close()
+
+	err := db.Create(s).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Server) Update() error {
+	db := openDB()
+	defer db.Close()
+
+	err := db.Model(s).Update(s).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func armaRoutes(router *gin.Engine, uri string) *gin.RouterGroup {
@@ -19,6 +47,18 @@ func armaRoutes(router *gin.Engine, uri string) *gin.RouterGroup {
 	armaRoute.GET("", func(c *gin.Context) {
 		c.JSON(200, gin.H{"data": map[string]interface{}{"hello": "world"}, "message": "It worked"})
 	})
+	armaRoute.GET("/:id", getServer)
+	armaRoute.POST("", newServer)
 
 	return armaRoute
+}
+
+func getServer(c *gin.Context) {
+	id := c.Param("id")
+	intID, err := strconv.Atoi(id)
+
+}
+
+func newServer(c *gin.Context) {
+
 }
