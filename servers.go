365ecae5c362c3503389c8d387dcb3113889ab38
Dixon <dixonbegay@gmail.com>
2019-04-16 08:27:21 -0700

    Polished up helpers for CRUD operations with servers

--- servers.go
+++ servers.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"strconv"
-	"time"
-
-	"github.com/shadez95/go-arma"
 
 	"github.com/gin-gonic/gin"
+	"github.com/shadez95/go-arma"
 )
 
+const (
+	// Active is a constant string that is used by Status
+	Active = "active"
+
+	// Inactive is a constant string that is used by Status
+	Inactive = "inactive"
+	// Starting is a constant string that is used by Status
+	Starting = "starting"
+)
+
+// Status type represents status of a server
+type Status string
+
+// Server type represents a server
 type Server struct {
-	ID        int    `gorm:"primary_key"`
-	Name      string `gorm:"unique;not null"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CustomGormModel
+	Name   string `gorm:"unique;not null"`
+	Status Status
 	arma.Server
 }
+
+var server *Server
 
 func createServer(s *Server) error {
 	db := openDB()
@@ -29,11 +42,11 @@ func createServer(s *Server) error {
 	return nil
 }
 
-func (s *Server) Update() error {
+func updateServer(s *Server) error {
 	db := openDB()
 	defer db.Close()
 
-	err := db.Model(s).Update(s).Error
+	err := db.Model(&s).Update(s).Error
 	if err != nil {
 		return err
 	}
@@ -41,24 +54,54 @@ func (s *Server) Update() error {
 	return nil
 }
 
-func armaRoutes(router *gin.Engine, uri string) *gin.RouterGroup {
-	armaRoute := router.Group(uri)
+func deleteServer(s *Server) error {
+	db := openDB()
+	defer db.Close()
 
-	armaRoute.GET("", func(c *gin.Context) {
-		c.JSON(200, gin.H{"data": map[string]interface{}{"hello": "world"}, "message": "It worked"})
-	})
-	armaRoute.GET("/:id", getServer)
-	armaRoute.POST("", newServer)
-
-	return armaRoute
+	err := db.Delete(&s).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
-func getServer(c *gin.Context) {
+func findServerByID(id int) (*Server, error) {
+	db := openDB()
+	defer db.Close()
+
+	err := db.Where("id = ?", id).First(&server).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return server, nil
+}
+
+func findServerByName(name string) (*Server, error) {
+	db := openDB()
+	defer db.Close()
+
+	err := db.Where("name = ?", name).First(&server).Error
+	if err != nil {
+		return nil, err
+	}
+	return server, nil
+}
+
+func getServer(c *gin.Context) (*Server, error) {
 	id := c.Param("id")
 	intID, err := strconv.Atoi(id)
+	if err != nil {
+		return nil, err
+	}
 
-}
+	db := openDB()
+	defer db.Close()
 
-func newServer(c *gin.Context) {
+	db.Where("id = ?", intID).First(&server)
+	if err != nil {
+		return nil, err
+	}
 
+	return server, nil
 }
